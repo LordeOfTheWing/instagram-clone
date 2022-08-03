7d0fc0948219806022314bc4b477ef51f1072bce
Clinton Mainya <klyntonmainya@gmail.com>
2022-08-03 08:22:07 +0300

    Create an indivual story component

---
+++ components/Story.tsx
@@ -0,0 +1,27 @@
+import { User } from "../types";
+
+interface Props {
+  profile: User;
+}
+
+const Story = ({ profile }: Props) => {
+  return (
+    <div>
+      <img
+        className='h-14 w-14 rounded-full p-[1.5px] 
+        border-red-500 border-2 object-contain 
+        cursor-pointer hover:scale-110 transition
+        transform duration-200 ease-out
+        '
+        src={profile.avatar}
+        alt='Profile pic'
+      />
+      <p
+        className='text-xs w-14 truncate 
+      text-center'>
+        {profile.userName}
+      </p>
+    </div>
+  );
+};
+export default Story;
