0ab82b4a45c707b77240cc61e6fd7de2c5e23620
Clinton Mainya <klyntonmainya@gmail.com>
2022-08-03 08:18:33 +0300

    Create a stories component

---
+++ components/Stories.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { User } from "../types";
+import { createRandomUser, Users } from "../utils/fakeUsers";
+import Story from "./Story";
+
+const Stories = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    Array.from({ length: 10 }).forEach(() => {
+      Users.push(createRandomUser());
+    });
+    setUsers(Users);
+  }, [Users]);
+
+  return (
+    <div
+      className='flex space-x-2 p-6 bg-[white] mt-8
+     border-gray-200 border rounded-sm 
+     overflow-x-scroll scrollbar-thin
+      scrollbar-thumb-black'>
+      {users.map((profile) => {
+        return <Story key={profile.userId} profile={profile} />;
+      })}
+    </div>
+  );
+};
+export default Stories;
