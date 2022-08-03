0d9f5f93a027e720c08b909cda55c7ae69e0c6e0
Clinton Mainya <klyntonmainya@gmail.com>
2022-08-03 19:43:32 +0300

    Prevent ablity to comment and like on posts if not having a session

--- components/Post.tsx
+++ components/Post.tsx
@@ -8,20 +8,18 @@ import {
 } from "@heroicons/react/outline";
 
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
 
 interface Post {
-  id: number;
+  id: string;
   username: string;
   userImg: string;
   img: string;
   caption: string;
 }
 
-interface Props {
-  post: Post;
-}
-
-const Post = ({ post }: Props) => {
+const Post = ({ id, caption, userImg, username, img }: Post) => {
+  const { data: session } = useSession();
   return (
     <div className='bg-white my-7 rounded-sm border'>
       {/* header */}
@@ -31,48 +29,52 @@ const Post = ({ post }: Props) => {
           className='rounded-full h-12 w-12
           object-contain p-1 mr-3 
           '
-          src={post.userImg}
+          src={userImg}
           alt='Post Profile'
         />
-        <p className='flex-1 font-bold'>{post.username}</p>
+        <p className='flex-1 font-bold'>{username}</p>
         <DotsHorizontalIcon className='h-5 cursor-pointer' />
       </div>
 
       {/* img */}
-      <img className='oject-cover w-full' src={post.img} alt='Post image' />
+      <img className='oject-cover w-full' src={img} alt='Post image' />
 
       {/* Buttons */}
-      <div className='flex justify-between px-4 pt-4'>
-        <div className='flex items-center space-x-4'>
-          <HeartIcon className='btnPost' />
-          <ChatIcon className='btnPost' />
-          <PaperAirplaneIcon className='btnPost' />
-        </div>
+      {session && (
+        <div className='flex justify-between px-4 pt-4'>
+          <div className='flex items-center space-x-4'>
+            <HeartIcon className='btnPost' />
+            <ChatIcon className='btnPost' />
+            <PaperAirplaneIcon className='btnPost' />
+          </div>
 
-        <BookmarkIcon className='btnPost' />
-      </div>
+          <BookmarkIcon className='btnPost' />
+        </div>
+      )}
       {/* caption */}
       <p className='p-5 truncate'>
-        <span className='font-bold mr-1'>{post.username} </span>
-        {post.caption}
+        <span className='font-bold mr-1'>{username} </span>
+        {caption}
       </p>
 
       {/* comments  */}
 
       {/* input box */}
-      <form className='flex items-center p-4'>
-        <EmojiHappyIcon className='h-7' />
-        <input
-          placeholder='Add a comment...'
-          type='text'
-          className='border-none flex-1 focus:ring-0 outline-none'
-        />
-        <button
-          className='font-semibold text-blue-400
-        '>
-          Post
-        </button>
-      </form>
+      {session && (
+        <form className='flex items-center p-4'>
+          <EmojiHappyIcon className='h-7' />
+          <input
+            placeholder='Add a comment...'
+            type='text'
+            className='border-none flex-1 focus:ring-0 outline-none'
+          />
+          <button
+            className='font-semibold text-blue-400
+          '>
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 };
