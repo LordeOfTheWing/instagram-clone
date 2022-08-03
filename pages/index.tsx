cbb2b5ae7a57a49c7eaabc8ea38332b00db30d5d
Clinton Mainya <klyntonmainya@gmail.com>
2022-08-03 08:22:33 +0300

    Create and add various components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div
+      className='bg-gray-50 h-screen overflow-y-scroll
+    scrollbar-hide
+    '>
       <Head>
         <title>Instagram - Clone</title>
         <link rel='icon' href='/favicon.ico' />
@@ -12,7 +16,7 @@ const Home: NextPage = () => {
 
       <Header />
 
-      {/* <Feed /> */}
+      <Feed />
 
       {/* <Modal/> */}
     </div>
